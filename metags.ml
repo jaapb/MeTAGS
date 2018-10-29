46ab7fd15af0fd33973a1f255257dd98dc5d39c8
Jaap Boender <jaapb@kerguelen.org>
2018-10-29 23:48:40 +0000

    Starting to work on argument parsing

--- metags.ml
+++ metags.ml
@@ -106,6 +106,7 @@ let redraw (dr: GDraw.drawable) l_timer l_turn ev =
 	false
 
 let _ =
+	Arg.parse arg_spec (fun s -> file_names := s::!file_names) "MeTAGS";
 	GtkMain.Main.init ();
 	let w = GWindow.window ~show:true ~width:300 ~height:300 ~title:"MeTAGS" () in
 	w#event#connect#key_press ~callback:(keypress w);
